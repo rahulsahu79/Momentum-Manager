d2f2e8c29bcc75b4227f468e88a7db0c805ab975
mohit <mohit9479sahu@gmail.com>
2025-08-17 13:37:41 +0530

    todo api completed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "django_filters",
     "corsheaders",
     "todoapi",
 ]
@@ -56,7 +57,10 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.AllowAny",
-    ]
+
+    ],
+     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+     "PAGE_SIZE": 5
 }
 
 
